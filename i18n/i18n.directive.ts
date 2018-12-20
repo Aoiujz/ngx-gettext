945f3893955c8d5e3553ab750c09f010429123f0
麦当苗儿 <zuojiazi@vip.qq.com>
2018-12-20 23:02:29 +0800

    Test

--- i18n/i18n.directive.ts
+++ i18n/i18n.directive.ts
@@ -50,7 +50,7 @@ export class I18nDirective implements OnInit, OnDestroy {
         }
 
         this.subscription = this.I18n.onLanguageChange.subscribe(() => {
-            console.log(this.n);
+            console.log([msgid, this.n, this.plural, this.context, this.args]);
             if (this.n && this.plural) {
                 this.setContent(this.I18n.plural(Number(this.n), msgid, this.plural, this.args || [], this.context));
             } else {
