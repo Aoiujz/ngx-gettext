bd92c47b09d594bfd70bc237a1337c780d2ef0b1
麦当苗儿 <zuojiazi@vip.qq.com>
2018-12-20 13:54:43 +0800

    change name

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@
  */
 
 import { I18nDirective } from "./i18n/i18n.directive";
-import { I18nModule } from "./i18n/i18n.module";
+import { I18nsModule } from "./i18n/i18n.module";
 import { I18nService } from "./i18n/i18n.service";
 
-export { I18nDirective, I18nModule, I18nService };
+export { I18nDirective, I18nsModule, I18nService };
