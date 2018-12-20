8ea53ada6ec50976178666e85508bca35d74d234
麦当苗儿 <zuojiazi@vip.qq.com>
2018-12-20 15:59:09 +0800

    Improve ngx-gettext

--- i18n/i18n.directive.ts
+++ i18n/i18n.directive.ts
@@ -3,35 +3,76 @@
  * @author zuojiazi@vip.qq.com
  */
 
-import { Directive, ElementRef, OnInit, Input } from '@angular/core';
+import { Directive, ElementRef, OnInit, Input, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { I18nService } from './i18n.service';
 
 @Directive({
     selector: '[i18ns]',
 })
-export class I18nDirective implements OnInit {
+export class I18nDirective implements OnInit, OnDestroy {
     @Input()
     private i18ns: string;
-    private msgid: string;
+
+    @Input('i18n-n')
+    private n: string;
+
+    @Input('i18n-plural')
+    private plural: string;
+
+    @Input('i18n-args')
+    private args: any[];
+
+    @Input('i18n-context')
+    private context: string;
 
     private element: HTMLElement;
+    private subscription: Subscription;
 
     constructor(
         element: ElementRef,
+        private I18n: I18nService,
     ) {
         this.element = element.nativeElement as HTMLElement;
     }
 
     ngOnInit() {
-        if (this.i18ns) {
-            this.msgid = this.element.getAttribute(this.i18ns);
-        } else {
-            this.msgid = this.element.textContent;
+        const msgid = this.getMsgid();
+
+        if (this.n && !/\d+/.test(this.n)) {
+            throw new Error('Attribute "i18n-n" must be a numeric string');
         }
 
-        console.log(this.element.textContent);
-        console.log(this.element.innerText);
-        console.log(this.element.innerHTML);
+        if (this.args && !Array.isArray(this.args)) {
+            throw new Error('Attribute "i18n-args" must be an array');
+        }
 
-        console.log(this.msgid);
+        this.subscription = this.I18n.onLanguageChange.subscribe(() => {
+            if (this.n && this.plural) {
+                this.setContent(this.I18n.plural(Number(this.n), msgid, this.plural, this.args || [], this.context));
+            } else {
+                this.setContent(this.I18n.get(msgid, this.args || [], this.context));
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+
+    private getMsgid() {
+        if (this.i18ns) {
+            return this.element.getAttribute(this.i18ns);
+        } else {
+            return this.element.textContent;
+        }
+    }
+
+    private setContent(message: string) {
+        if (this.i18ns) {
+            this.element.setAttribute(this.i18ns, message);
+        } else {
+            this.element.textContent = message;
+        }
     }
 }
